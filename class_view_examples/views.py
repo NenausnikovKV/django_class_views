ed873ceeae20f11b4e563f9a7f1565a8a96a2fc2
kostya <konstantin2113@mail.ru>
2024-02-03 13:37:13 +0300

    clare comments

--- class_view_examples/views.py
+++ class_view_examples/views.py
@@ -11,20 +11,6 @@ from .models import Publisher, Author
 
 class RootPage(TemplateView):
 
-    # addresses = {
-    #     "standard_methods/": reverse_lazy("class_view_examples:standard_method"),
-    #     "simple_template/": reverse_lazy("class_view_examples:simple_template"),
-    #     "redirection/": reverse_lazy("class_view_examples:class_redirect_view"),
-    #     "redirect_stump/": reverse_lazy("class_view_examples:redirect_page"),
-    #     "publisher_list/": reverse_lazy("class_view_examples:publisher_view"),
-    #     "publisher_detail/<int:pk>/": reverse_lazy("class_view_examples:publisher_detail"),
-    #     "contact_form/": reverse_lazy("class_view_examples:contact_form"),
-    #     "author_detail/<int:pk>/": reverse_lazy("class_view_examples:author_detail"),
-    #     "author/add/": reverse_lazy("class_view_examples:author_add"),
-    #     "author/<int:pk>/": reverse_lazy("class_view_examples:author_update"),
-    #     "author/<int:pk>/delete/": reverse_lazy("class_view_examples:author_delete"),
-    # }
-
     template_name = "class_view_examples/root_template.html"
     # extra_context = {"addresses": addresses}
 
